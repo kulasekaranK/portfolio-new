be50bd2a11f40f1990cda85dfed43b9c171c65f6
KULASEKARAN <itzkulasekaran@gmail.com>
2025-10-30 16:45:05 +0530

    Remove SectionHeader import from About component
    Removed SectionHeader import from About component.

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,9 +1,8 @@
 import { Component, HostListener } from '@angular/core';
-import { SectionHeader } from "../../shared/ui/section-header/section-header";
 
 @Component({
   selector: 'app-about',
-  imports: [SectionHeader],
+  imports: [],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
